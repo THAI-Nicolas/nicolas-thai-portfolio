84f788a24c5d2c5b3d82e0705685aac93a41d3c8
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2025-11-17 01:46:32 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     ssr: {
-      noExternal: ['pocketbase'],
+      noExternal: ["pocketbase"],
     },
   },
   output: "server",
